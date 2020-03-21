b6cfe5d8484ae7d3893311ea66fdcf3a7f231c9d
Ana Fernández <ana.fdez.mir@gmail.com>
2020-03-21 22:02:31 +0100

    table for vendors

--- frontend/src/app/views/vendors-list/vendors-list.component.ts
+++ frontend/src/app/views/vendors-list/vendors-list.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface Vendor {
+  name: string;
+  zipCode: string;
+  address: string;
+}
+
+const ELEMENT_DATA: Vendor[] = [ // TODO
+  { name: "Rapp Gemüse", zipCode: "73462", address: "Meine Straße 234, Musterstadt"},
+  { name: "Edeka", zipCode: "73461", address: "Kleine Gasse 34731, Musterstadt"},
+  { name: "Rewe", zipCode: "32567", address: "Am schönen Rain 34, Irgendwo"},
+];
 
 @Component({
   selector: 'app-vendors-list',
@@ -6,10 +20,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendors-list.component.scss']
 })
 export class VendorsListComponent implements OnInit {
+displayedColumns: string[] = ['name', 'address'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  constructor() { }
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.dataSource.sort = this.sort;
   }
 
+   applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
