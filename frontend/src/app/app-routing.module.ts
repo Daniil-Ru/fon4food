86ac1a4b46f480a641363062ab46f5ee969ce8fc
Ana Fernández <ana.fdez.mir@gmail.com>
2020-03-21 20:30:04 +0100

    added some guards

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DeliveryComponent } from './views/delivery/delivery.component';
+import { DeliveryGuard } from './views/delivery/delivery.guard';
 import { DeliveryOrderComponent } from './views/delivery/order/delivery-order.component';
 import { LoginComponent } from './views/login/login.component';
 import { StartComponent } from './views/start/start.component';
 import { VendorOrderComponent } from './views/vendor/order/vendor-order.component';
 import { VendorComponent } from './views/vendor/vendor.component';
+import { VendorGuard } from './views/vendor/vendor.guard';
 import { VendorsListComponent } from './views/vendors-list/vendors-list.component';
 
 
@@ -14,13 +16,19 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'vendors-list', component: VendorsListComponent },
   {
-    path: 'vendor', component: VendorComponent, children: [
-      { path: 'order', component: VendorOrderComponent },
+    path: 'vendor',
+    component: VendorComponent,
+    canActivate: [VendorGuard],
+    children: [
+      { path: 'order', component: VendorOrderComponent, canActivate: [VendorGuard] },
     ],
   },
   {
-    path: 'delivery', component: DeliveryComponent, children: [
-      { path: 'order', component: DeliveryOrderComponent },
+    path: 'delivery',
+    component: DeliveryComponent,
+    canActivate: [DeliveryGuard],
+    children: [
+      { path: 'order', component: DeliveryOrderComponent, canActivate: [DeliveryGuard] },
     ],
   },
   { path: '**', redirectTo: '/start', pathMatch: 'full' },
