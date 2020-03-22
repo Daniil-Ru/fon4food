8c412486f3fc69300ac374de5af6e4de11a69c1c
Ana Fernández <ana.fdez.mir@gmail.com>
2020-03-22 23:02:41 +0100

    using REST interface for vendors list

--- frontend/src/app/views/vendors-list/vendors-list.component.ts
+++ frontend/src/app/views/vendors-list/vendors-list.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -12,28 +13,30 @@ export interface Vendor {
   deliveryFlatRate: string;
 }
 
-const ELEMENT_DATA: Vendor[] = [ // TODO
-  { name: "Max Gemüse", zipCode: "73462", address: "Meine Straße 234", city:'Musterstadt', phone: '0789 890', orderTimes: 'Mo', deliveryFlatRate: '10€'},
-  { name: "MusterMarkt", zipCode: "73461", address: "Kleine Gasse 34731", city:'Musterstadt', phone: '(2)435 890', orderTimes: '8:0- 8:30', deliveryFlatRate: '2€'},
-  { name: "MeinSupermarkt", zipCode: "32567", address: "Am schönen Rain 34", city:'Irgendwo', phone: '3 890', orderTimes: 'Mo', deliveryFlatRate: '1.5€'},
-];
-
 @Component({
   selector: 'app-vendors-list',
   templateUrl: './vendors-list.component.html',
-  styleUrls: ['./vendors-list.component.scss']
+  styleUrls: ['./vendors-list.component.scss'],
 })
 export class VendorsListComponent implements OnInit {
-displayedColumns: string[] = ['name', 'address', 'phone', 'orderTimes', 'deliveryFlatRate'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns: string[] = ['name', 'address', 'phone', 'orderTimes', 'deliveryFlatRate'];
+  dataSource = new MatTableDataSource([]);
 
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  ngOnInit() {
-    this.dataSource.sort = this.sort;
+  constructor(readonly http: HttpClient) {
   }
 
-   applyFilter(event: Event) {
+  ngOnInit() {
+    this.dataSource.sort = this.sort;
+
+    this.http.get('api/vendors')
+      .subscribe((vendors: Vendor[]) => {
+        this.dataSource = new MatTableDataSource(vendors);
+      });
+  }
+
+  applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
