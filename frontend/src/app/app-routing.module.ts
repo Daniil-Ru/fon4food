9c0fbbc833499fea195e3ae23c6340b232aaddd7
Ana Fernández <ana.fdez.mir@gmail.com>
2020-04-05 11:14:38 +0200

    guard for start page added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DeliveryComponent } from './views/delivery/delivery.component';
 import { DeliveryGuard } from './views/delivery/delivery.guard';
 import { DeliveryOrderComponent } from './views/delivery/order/delivery-order.component';
 import { LoginComponent } from './views/login/login.component';
 import { AboutComponent } from './views/about/about.component';
+import { StartGuard } from './views/start/start.guard';
 import { VendorOrderComponent } from './views/vendor/order/vendor-order.component';
-import { VendorComponent } from './views/vendor/vendor.component';
 import { VendorGuard } from './views/vendor/vendor.guard';
 import { VendorsListComponent } from './views/vendors-list/vendors-list.component';
 import { StartComponent } from './views/start/start.component';
@@ -14,9 +13,9 @@ import { StartComponent } from './views/start/start.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
-  { path: 'start', component: StartComponent },
-  { path: 'login', component: LoginComponent },
   { path: 'vendors', component: VendorsListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'start', component: StartComponent, canActivate: [StartGuard] },
   { path: 'vendor/order', component: VendorOrderComponent, canActivate: [VendorGuard] },
   { path: 'vendor/order/:id', component: VendorOrderComponent, canActivate: [VendorGuard] },
   { path: 'delivery/:id', component: DeliveryOrderComponent, canActivate: [DeliveryGuard] },
