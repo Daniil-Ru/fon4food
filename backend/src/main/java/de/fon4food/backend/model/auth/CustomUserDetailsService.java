6f5ee1fe5c9333ac4eddd1313eaa98d68c8a657a
Johannes Beichter <jbeichter@gmx.de>
2020-04-16 20:11:35 +0200

    email as username

--- backend/src/main/java/de/fon4food/backend/model/auth/CustomUserDetailsService.java
+++ backend/src/main/java/de/fon4food/backend/model/auth/CustomUserDetailsService.java
@@ -14,7 +14,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) {
-		User user = userRepository.findByUsername(username);
+		User user = userRepository.findByEmail(username);
 		if (user == null) {
 			throw new UsernameNotFoundException(username);
 		}
--- backend/src/main/java/de/fon4food/backend/model/auth/CustomUserPrincipal.java
+++ backend/src/main/java/de/fon4food/backend/model/auth/CustomUserPrincipal.java
@@ -34,7 +34,7 @@ public class CustomUserPrincipal implements UserDetails {
 
 	@Override
 	public String getUsername() {
-		return this.user.getUsername();
+		return this.user.getEmail();
 	}
 
 	@Override
