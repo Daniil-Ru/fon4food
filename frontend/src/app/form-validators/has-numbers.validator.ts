8b42ff4983699069a2034e8fb90fb20e59144b2c
Ana Fernández <ana.fdez.mir@gmail.com>
2020-04-19 23:01:46 +0200

    modified regex for numbers validator

--- frontend/src/app/form-validators/has-numbers.validator.ts
+++ frontend/src/app/form-validators/has-numbers.validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, FormGroup, ValidatorFn } from '@angular/forms';
 
 export function hasNumbersValidator(): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
-    if (!control.value.match(/\d/)) {
+    if (!control.value.match(/[0-9]/)) {
       return { numbers: { value: true } };
     }
 
